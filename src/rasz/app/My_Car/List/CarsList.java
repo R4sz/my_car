4f79ab4d9f8be3e3fd9bad7333125c5640c50798
R4sz <cddev02@gmail.com>
2015-06-14 15:27:03 +0200

    .

--- src/rasz/app/My_Car/List/CarsList.java
+++ src/rasz/app/My_Car/List/CarsList.java
@@ -8,6 +8,7 @@ import rasz.app.My_Car.SamochodyDetails;
 import rasz.app.My_Car.SamochodyDodaj;
 import rasz.app.My_Car.SamochodyEdit;
 import rasz.app.My_Car.repository.CarsRepository;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
@@ -24,95 +25,103 @@ import android.widget.ListView;
 
 public class CarsList extends Activity {
 
-	public static CarsRepository entry = new CarsRepository();
+    private final static int CONTEXT_EDIT = 0;
+    private final static int CONTEXT_DELETE = 1;
+    private final static int CONTEXT_ADD = 2;
 
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.samochody_lista);
-		initListView();
-	}
 
-	protected void onRestart() {
-		super.onRestart();
-		DataContainer.getDataFromDB(this, "SELECT * FROM Samochody", "cars");
-		initListView();	
-	}
-	
-	private void initListView() {
-		
-		TextView active = (TextView) findViewById(R.id.active_cars);
-		String aktywne = "Aktywne Samochody:";
-		for (int i = 0; i < DataContainer.listOfCars.size(); i++) {
+    public static CarsRepository entry = new CarsRepository();
 
-			entry = DataContainer.listOfCars.get(i);
-			if (entry.getAktywny() == 1) {
-				aktywne += " " + entry.getCarName();
-			}
-		} 
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.samochody_lista);
+        initListView();
+    }
 
-		if (!aktywne.equals("Aktywne Samochody:")) {		
-			active.setText(aktywne);
-		} else {
-			active.setText("Brak aktywnych samochod�w");
-		}
+    protected void onRestart() {
+        super.onRestart();
+        DataContainer.getDataFromDB(this, "SELECT * FROM Samochody", "cars");
+        initListView();
+    }
 
-		ListView list = (ListView) findViewById(R.id.ListView01);
-		list.setClickable(true);
+    private void initListView() {
 
-		SamochodyCarAdapter adapter = new SamochodyCarAdapter(this, DataContainer.listOfCars);
+        TextView active = (TextView) findViewById(R.id.active_cars);
+        String aktywne = "Aktywne Samochody:";
+        for (int i = 0; i < DataContainer.listOfCars.size(); i++) {
 
-		list.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> arg0, View view, int position, long index) {
-				Intent td = new Intent(getApplicationContext(), SamochodyDetails.class);
-				td.putExtra("position", position);
-				startActivity(td);
-			}
-		});
-		list.setAdapter(adapter);
-		registerForContextMenu(list);
-	}
+            entry = DataContainer.listOfCars.get(i);
+            if (entry.getAktywny() == 1) {
+                aktywne += " " + entry.getCarName();
+            }
+        }
 
-	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
-		if (v.getId() == R.id.ListView01) {
-			menu.setHeaderTitle("Opcje...");
-			String[] menuItems = getResources().getStringArray(R.array.menu);
-			for (int i = 0; i < menuItems.length; i++) {
-				menu.add(Menu.NONE, i, i, menuItems[i]);
-			}
-		}
-	}
+        if (!aktywne.equals("Aktywne Samochody:")) {
+            active.setText(aktywne);
+        } else {
+            active.setText("Brak aktywnych samochod�w");
+        }
 
-	public boolean onContextItemSelected(MenuItem item) {
-		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-		int menuItemIndex = item.getItemId();
+        ListView list = (ListView) findViewById(R.id.ListView01);
+        list.setClickable(true);
 
-		int idListForExtraMenu = info.position;
-		entry = DataContainer.listOfCars.get(idListForExtraMenu);
-		int idCar = entry.getIdsam();
+        SamochodyCarAdapter adapter = new SamochodyCarAdapter(this, DataContainer.listOfCars);
 
-		DataContainer.database = new DatabaseDaneDB(this);
-		SQLiteDatabase bd = DataContainer.database.getReadableDatabase();
-		// menuItemIndex (id rekordu listy z dlugiego klika)
-		// menuItemIndex = 2 add;
-		// menuItemIndex = 1 delete;
-		// menuItemIndex = 0 edit;		
-		if (menuItemIndex == 1) {
-			bd.delete("samochody", "idsam" + "=" + idCar, null);
-			DataContainer.database.close();
-			DataContainer.listOfCars.remove(idListForExtraMenu);
-			initListView();
-		} else if (menuItemIndex == 0) {
-			Intent k = new Intent(this, SamochodyEdit.class);
-			k.putExtra("position", idListForExtraMenu);
-			startActivity(k);
-		} else if (menuItemIndex == 2) {
-			Intent l = new Intent(this, SamochodyDodaj.class);
-			startActivity(l);
-		}
-		return true;
-	}
+        list.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> arg0, View view, int position, long index) {
+                Intent td = new Intent(getApplicationContext(), SamochodyDetails.class);
+                td.putExtra("position", position);
+                startActivity(td);
+            }
+        });
+        list.setAdapter(adapter);
+        registerForContextMenu(list);
+    }
 
-	public void backButton(View v) {
-		finish();
-	}
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+        if (v.getId() == R.id.ListView01) {
+            menu.setHeaderTitle("Opcje...");
+            String[] menuItems = getResources().getStringArray(R.array.menu);
+            for (int i = 0; i < menuItems.length; i++) {
+                menu.add(Menu.NONE, i, i, menuItems[i]);
+            }
+        }
+    }
+
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        int menuItemIndex = item.getItemId();
+
+        int idListForExtraMenu = info.position;
+        entry = DataContainer.listOfCars.get(idListForExtraMenu);
+        int idCar = entry.getIdsam();
+
+        DataContainer.database = new DatabaseDaneDB(this);
+        SQLiteDatabase bd = DataContainer.database.getReadableDatabase();
+        switch (menuItemIndex) {
+            case CONTEXT_DELETE:
+                bd.delete("samochody", "idsam" + "=" + idCar, null);
+                DataContainer.database.close();
+                DataContainer.listOfCars.remove(idListForExtraMenu);
+                initListView();
+                break;
+
+            case CONTEXT_EDIT:
+                Intent k = new Intent(this, SamochodyEdit.class);
+                k.putExtra("position", idListForExtraMenu);
+                startActivity(k);
+                break;
+
+            case CONTEXT_ADD:
+                Intent l = new Intent(this, SamochodyDodaj.class);
+                startActivity(l);
+                break;
+        }
+
+        return true;
+    }
+
+    public void backButton(View v) {
+        finish();
+    }
 }
