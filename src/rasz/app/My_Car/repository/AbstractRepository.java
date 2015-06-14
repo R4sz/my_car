bbc3ee84567380b054ef727fb476306a039f7ee6
R4sz <cddev02@gmail.com>
2015-06-14 13:55:02 +0200

    .
    .

--- src/rasz/app/My_Car/repository/AbstractRepository.java
+++ src/rasz/app/My_Car/repository/AbstractRepository.java
@@ -1,6 +1,36 @@
 package rasz.app.My_Car.repository;
 
 public abstract class AbstractRepository {
-	
+
+	private int id;
+	private double mileAge;
+	private double cost;
+	private long timestamp;
+	private String place;
+	private String notes;
+
+	public int getId() {
+		return id;
+	}
+
+	public double getMileAge() {
+		return mileAge;
+	}
+
+	public double getCost() {
+		return cost;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
 
 }
